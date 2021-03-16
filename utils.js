38fb970bc5841eac2208734bbc0ad05b592630c6
Rob Osborn <rosborn@webstacks.com>
2021-03-16 10:28:13 -0700

    cleaned code base for production

--- utils.js
+++ utils.js
@@ -2,30 +2,6 @@ const axios = require('axios');
 require('dotenv').config()
 const { getCredential, addCredential, updateCredential } = require('./db')
 
-const getContacts = async (token, contacts, email, next) => {
-    try {
-        let url = `https://api.infusionsoft.com/crm/rest/v1/contacts?order=date_created&order_direction=descending`
-        if (next) {
-            url = `${next}`
-        }
-        const res = await axios.get(url, {
-            headers: {
-                'Authorization': `Bearer ${token}`, 'Accept': "application/json, */*"
-            }
-        })
-        for (let i = 0; i < res.data.contacts.length; i++) {
-            for (let j = 0; j < res.data.contacts[i].email_addresses.length; i++) {
-                if (res.data.contacts[i].email_addresses[j].email === email) {
-                    return res.data.contacts[i]
-                }
-            }
-        }
-        return
-    } catch (e) {
-        console.log(e);
-    }
-}
-
 const getContact = async (token, email) => {
     try {
         const res = await axios.get(`https://api.infusionsoft.com/crm/rest/v1/contacts?email=${email}&custom_fields`, {
@@ -42,49 +18,6 @@ const getContact = async (token, email) => {
     } catch (e) {
         console.log(e);
     }
-}
-
-const newGetContact = async (token, userId) => {
-    try {
-        const reqBody = {
-            optional_properties: ['custom_fields']
-        }
-        const res = await axios.get(`https://api.infusionsoft.com/crm/rest/v1/contacts/${userId}?optional_properties=custom_fields`, {
-            headers: {
-                'Authorization': `Bearer ${token}`, 'Accept': "application/json, */*"
-            }
-        })
-        return res.data
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-const getContactModel = async (token) => {
-    try {
-        const res = await axios.get(`https://api.infusionsoft.com/crm/rest/v1/contacts/model`, {
-            headers: {
-                'Authorization': `Bearer ${token}`, 'Accept': "application/json, */*"
-            }
-        })
-        console.log(res.data);
-        return res.data
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-const createCustomField = async (token) => {
-    const reqBody = {
-        "field_type": "Text",
-        "label": "Access Code",
-    }
-    const res = await axios.post(`https://api.infusionsoft.com/crm/rest/v1/contacts/model/customFields`, reqBody, {
-        headers: {
-            'Authorization': `Bearer ${token}`, 'Accept': "application/json, */*"
-        }
-    })
-    console.log("RESPONSe", res.data);
 }
 
 const updateContact = async (token, contactId, password) => {
@@ -108,28 +41,4 @@ const updateContact = async (token, contactId, password) => {
     }
 }
 
-const playground = async () => {
-    try {
-        const oldTokenData = await getCredential()
-        // const oldRefreshToken = 'NFJvmAvikO9P9VGCGRon2VnGk912AdXS'
-        const oldRefreshToken = oldTokenData[0].refresh_token
-        const data = `grant_type=refresh_token&refresh_token=${oldRefreshToken}`
-        const base64encoded = Buffer.from(`${process.env.KEAP_API_KEY}:${process.env.KEAP_SECRET}`).toString('base64')
-        const resData = await axios.post(`https://api.infusionsoft.com/token`, data, { headers: { 'Authorization': `Basic ${base64encoded}`, 'content-type': 'application/x-www-form-urlencoded' } })
-        const access_token = resData.data.access_token
-        const refresh_token = resData.data.refresh_token
-        console.log("access token", access_token, '\n', "refresh token", refresh_token);
-        await updateCredential(oldTokenData[0]._id, {
-            access_token,
-            refresh_token
-        })
-        const cont = await newGetContact(access_token, '647219')
-        console.log("CONTACT", cont);
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-// playground()
-
-module.exports = { getContact, getContactModel, updateContact }
+module.exports = { getContact, updateContact }
